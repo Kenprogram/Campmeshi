55e6c37f873ab80e8c58add111a173a5cc0cf7f8
Kenprogram <tadakenta@nakayukie-no-MacBook-Air.local>
2019-12-02 08:02:26 +0900

    gem_devise install

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,2 @@
 class Recipe < ApplicationRecord
-  belongs_to :category
-  belongs_to :user
-  belongs_to :material
-  belongs_to :how_to
 end
--- db/migrate/20191130221359_create_recipes.rb
+++ db/migrate/20191130221359_create_recipes.rb
@@ -1,12 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.references :category, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.string :catchcopy
-      t.references :material, null: false, foreign_key: true
-      t.references :how_to, null: false, foreign_key: true
+      t.string :title,       null: false
+      t.string :catchcopy,   null:false
 
       t.timestamps
     end
